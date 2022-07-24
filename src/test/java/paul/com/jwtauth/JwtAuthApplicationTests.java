5e1601838b679e7cd2213807f98c63301b533625
hamzapaul <73592443+hamzapaul@users.noreply.github.com>
2022-07-24 21:21:29 +0200

    project created

---
+++ src/test/java/paul/com/jwtauth/JwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package paul.com.jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
